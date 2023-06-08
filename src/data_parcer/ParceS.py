a11714853a292a5df1b95ee7fa2dc0e2ee738391
oleg <9ma1ka@mail.ru>
2023-06-08 16:57:02 +0300

    Add base parser and f

--- src/data_parcer/ParceS.py
+++ src/data_parcer/ParceS.py
@@ -1,5 +1,53 @@
-import bs4
 import requests
+import bs4
+from transliterate import translit
 
-class BaseParce:
-    
+
+class BaseParser:
+    def __init__(self, base_url: str) -> None:
+        """Initialize BaseParser object.
+
+        Args:
+            base_url (str): base domain for parsing.
+
+        Returns:
+            None
+        """
+        self.base_url = base_url
+
+    def parse_s(self, payload: str, headers: str):  # change (payload, headers) on class variables or object variables
+        """Target resource parsing method.
+
+        Args:
+            payload (str): Payloads.
+            headers (str): Headers.
+        Returns:
+            None
+        """
+        response = requests.request("GET", self.base_url, data=payload, headers=headers)
+        text = response.text
+        soup = bs4.BeautifulSoup(text, features='html.parser')
+        return soup
+
+
+class ForklogPars(BaseParser):
+    def parse_s(self, payload: str, headers: str):  # change (payload, headers) on class variables or object variables
+        """Target resource parsing method.
+
+        Args:
+            payload (str): Payloads.
+            headers (str): Headers.
+        Returns:
+            None
+        """
+        response = requests.request("GET", self.base_url, data=payload, headers=headers)
+        text = response.text
+        soup = bs4.BeautifulSoup(text, features='html.parser')
+        news_head = soup.find_all('div', class_='text_blk')
+        all_text = [t.text.strip().split('\n')[0] for t in news_head]
+        tranliterate_text = list(map(lambda x: translit(x.lower(), 'ru', reversed=True).replace(' ', '-'), all_text))
+        return tranliterate_text
+
+
+
+Fork_p = ForklogPars('https://forklog.com/news')
