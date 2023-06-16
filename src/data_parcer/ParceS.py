f102c788efa3b86d1fa577ee68012e95845ec46f
oleg <9ma1ka@mail.ru>
2023-06-16 13:19:27 +0300

    add coindesc parse

--- src/data_parcer/ParceS.py
+++ src/data_parcer/ParceS.py
@@ -21,6 +21,7 @@ class BaseParser:
         Args:
             payload (str): Payloads.
             headers (str): Headers.
+
         Returns:
             None
         """
@@ -31,14 +32,15 @@ class BaseParser:
 
 
 class ForklogPars(BaseParser):
-    def parse_s(self, payload: str, headers: str):  # change (payload, headers) on class variables or object variables
+    def parse_s(self, payload: str, headers: str) -> list[str]:  # change (payload, headers) on class variables or object variables
         """Target resource parsing method.
 
         Args:
             payload (str): Payloads.
             headers (str): Headers.
+
         Returns:
-            None
+            all_href_parse (list[str]): link to each article
         """
         response = requests.request("GET", self.base_url, data=payload, headers=headers)
         text = response.text
@@ -50,6 +52,38 @@ class ForklogPars(BaseParser):
         all_href_parse = list(map(lambda x: self.base_url + '/' + x.replace('ija', 'iya'), tranliterate_text)) # write parsing tests for these links
         return all_href_parse
 
+class CoDePars(BaseParser):
+    def __init__(self, base_url: str, data_url: str) -> None:
+        """Initialize CoDePars object.
+
+        Args:
+            base_url (str): base domain for parsing.
+            data_url (str): url for artickle concatnate
+
+        Returns:
+            None
+        """
+        super().__init__(base_url)
+        self.data_url = data_url
+
+    def parse_s(self, payload: str, headers: str) -> list[str]:  # change (payload, headers) on class variables or object variables
+        """Target resource parsing method.
+
+        Args:
+            payload (str): Payloads.
+            headers (str): Headers.
+
+        Returns:
+            all_href_parse (list[str]): link to each article
+        """
+        response = requests.request("GET", self.base_url, data=payload, headers=headers)
+        text = response.text
+        soup = bs4.BeautifulSoup(text, features='html.parser')
+        articles_2 = soup.find_all('a', class_="headline__HeadlineLink-sc-1uoawmp-0 jbDMkW")
+        all_href_parse = [self.data_url + t['href'] for t in articles_2]
+        return all_href_parse
+
 
 
 Fork_p = ForklogPars('https://forklog.com/news')
+CoDe_p = CoDePars('https://www.coindesk.com/livewire/', 'https://www.coindesk.com')
